13a91f364291a1769e4b1b3560c5ce111ef54943
Carlos Seguin-Lozano <donseguin@gmail.com>
2024-03-20 16:19:07 +0000

    ci(DB conn): get SSL Cert from env

--- src/database.py
+++ src/database.py
@@ -1,8 +1,10 @@
-from typing import Annotated
+import os
 import ssl
+from typing import Annotated
+
 from fastapi import Depends
 from sqlalchemy import MetaData
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session, sessionmaker
 
@@ -11,16 +13,16 @@ from src.constants import DB_NAMING_CONVENTION
 
 DATABASE_URL = str(settings.DATABASE_URL)
 
+# Get the SSL certificate path from Render.com environment variable or fallback to local
+ssl_cert_path = os.getenv("SSL_CERT_PATH", "/etc/ssl/cert.pem")
+
 # Create an SSL context
-ssl_context = ssl.create_default_context(cafile="/etc/ssl/cert.pem")
+ssl_context = ssl.create_default_context(cafile=ssl_cert_path)
 ssl_context.check_hostname = True  # Enable hostname verification
 ssl_context.verify_mode = ssl.CERT_REQUIRED  # Ensure the certificate is required
 
 # Create the async engine with the SSL context
-engine = create_async_engine(
-    DATABASE_URL,
-    connect_args={"ssl": ssl_context}
-)
+engine = create_async_engine(DATABASE_URL, connect_args={"ssl": ssl_context})
 
 
 # Create a sessionmaker instance with AsyncSession
