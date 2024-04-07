0ff6f94eb26c8daaa6411d410ec4eca997fedd3b
Carlos Seguin-Lozano <donseguin@gmail.com>
2024-04-07 17:45:02 +0100

    chore(i18n): Nuevos canales officiales  whatsapp

--- src/config.py
+++ src/config.py
@@ -13,8 +13,6 @@ class Config(BaseSettings):
     SITE_DOMAIN: str
     TEST_CHANNEL_ES: str
     TEST_CHANNEL_PT: str
-    WHATSAPP_CHANNEL_ES: str
-    WHATSAPP_CHANNEL_PT: str
     WHAPI_BEARER: str
     WHAPI_BASE_URL: str
     WHAPI_TIMEOUT: int = 300  # Timeout in seconds
