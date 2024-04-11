13d11670621a785a76d29390aeae2d7dc257dd09
Carlos Seguin-Lozano <donseguin@gmail.com>
2024-04-11 08:53:50 +0100

    fix(web_sources): read task now passes DB context to tasks

--- src/web_sources/tasks.py
+++ src/web_sources/tasks.py
@@ -1,16 +1,18 @@
+# src/web_sources/tasks.py
+from src.database import get_db
 from src.utils import setup_logger
 from src.web_sources.adidas.adidas import scrape_adidas
 from src.web_sources.carhartt.carhartt import scrape_carhartt
 from src.web_sources.converse.converse import scrape_converse
 from src.web_sources.nike.nike import scrape_nike
 
-# Initialize logger
 logger = setup_logger(__name__)
 
 
 async def read_sources_task():
-    logger.info("Reading sources task started...")
-    await scrape_adidas()
-    await scrape_carhartt()
-    await scrape_converse()
-    await scrape_nike()
+    async for db in get_db():
+        logger.info("Reading sources task started...")
+        await scrape_adidas(db)
+        await scrape_carhartt(db)
+        await scrape_converse(db)
+        await scrape_nike(db)
